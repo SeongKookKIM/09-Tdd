b20bf7eeeda36f1df58716e02f07596b30bd008e
SeongKookKIM <ksk8646@naver.com>
2024-07-10 10:37:41 +0900

    Initialize Test Project

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by SeongKook on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
