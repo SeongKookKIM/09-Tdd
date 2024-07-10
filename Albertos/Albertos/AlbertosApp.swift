d579acfab99fe10e2e99c3a38b0db72f880b65c7
SeongKookKIM <ksk8646@naver.com>
2024-07-10 13:07:35 +0900

    Initialize AlbertosTestApp

---
+++ Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by SeongKook on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
