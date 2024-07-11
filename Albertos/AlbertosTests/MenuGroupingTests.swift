66269bda45797c2ffbc018840f915b0e0f3cddd5
SeongKookKIM <ksk8646@naver.com>
2024-07-11 09:48:42 +0900

    타입 지정으로 fixture 사용을 간결하고 명확하게 변경

--- Albertos/AlbertosTests/MenuGroupingTests.swift
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -9,17 +9,14 @@ import XCTest
 @testable import Albertos
 
 final class MenuGroupingTests: XCTestCase {
-    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
-        
-    }
     
-    func testMenuWithOneCategoryReturnOneSection() {
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategoryInReverseAlphabeticalOrder() {
         // 메뉴아이템이 동일한 카테고리를 같는 경우 하나의 섹션으로 구분
-        let menu = [
-            MenuItem.fixture(category: "pastas", name: "a pasta"),
-            MenuItem.fixture(category: "drinks", name: "a drink"),
-            MenuItem.fixture(category: "desserts", name: "a dessert"),
-            MenuItem.fixture(category: "pastas", name: "another pasta"),
+        let menu: [MenuItem] = [
+            .fixture(category: "pastas", name: "a pasta"),
+            .fixture(category: "drinks", name: "a drink"),
+            .fixture(category: "desserts", name: "a dessert"),
+            .fixture(category: "pastas", name: "another pasta"),
         ].shuffled()
         let sections = groupMenuByCategory(menu)
         XCTAssertEqual(sections.count, 3)
@@ -29,6 +26,9 @@ final class MenuGroupingTests: XCTestCase {
         // XCTAssertEqual failed: // ("nil") is not equal to ("Optional("drinks")")
         XCTAssertEqual(sections[safe: 2]?.category, "desserts")
         // XCTAssertEqual failed: // ("nil") is not equal to ("Optional("desserts")")
+    }
+    
+    func testMenuWithOneCategoryReturnOneSection() {
     }
     
     func testEmptyMenuReturnsEmptySections() {
