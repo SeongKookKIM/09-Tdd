d34421f4d7988bb40cc645b534b71356fd26bd9e
SeongKookKIM <ksk8646@naver.com>
2024-07-12 10:19:24 +0900

    MenuFetcher 작성

---
+++ Albertos/AlbertosTests/MenuFetcherTests.swift
@@ -0,0 +1,14 @@
+//
+//  MenuFetcherTests.swift
+//  AlbertosTests
+//
+//  Created by SeongKook on 7/12/24.
+//
+
+import XCTest
+@testable import Albertos
+
+final class MenuFetcherTests: XCTestCase {
+    func testWhenRequestSucceedsPublishesDecodedMenuItems() {}
+    func testWhenRequestFailsPublishesReceivedError() {}
+}
