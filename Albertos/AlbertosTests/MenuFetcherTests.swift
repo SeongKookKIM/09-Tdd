7786dc2b4f78c476164c4108d597eb35880f92f0
SeongKookKIM <ksk8646@naver.com>
2024-07-12 10:22:10 +0900

    MenuFetcher 테스트 코드 작성

--- Albertos/AlbertosTests/MenuFetcherTests.swift
+++ Albertos/AlbertosTests/MenuFetcherTests.swift
@@ -6,9 +6,26 @@
 //
 
 import XCTest
+import Combine
 @testable import Albertos
 
 final class MenuFetcherTests: XCTestCase {
-    func testWhenRequestSucceedsPublishesDecodedMenuItems() {}
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func testWhenRequestSucceedsPublishesDecodedMenuItems() {
+        let menuFetcher = MenuFetcher()
+        let expectation = XCTestExpectation(description: "Publishes decoded [MenuItem]")
+        
+        menuFetcher.fetchMenu()
+            .sink(receiveCompletion: { _ in }, receiveValue: {
+                item in
+                // TODO: items값이 올바른지 테스트
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 1)
+    }
     func testWhenRequestFailsPublishesReceivedError() {}
 }
